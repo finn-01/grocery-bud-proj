37ebee4bb8ec51393632a86ef07f1b21b32c3e5b
finn-01 <nguyensam576@gmail.com>
2022-01-12 00:06:18 +0700

    set-up

---
+++ src/Alert.js
@@ -0,0 +1,7 @@
+import React, { useEffect } from "react";
+
+const Alert = () => {
+	return <div>Hello Alert</div>;
+};
+
+export default Alert;
---
+++ src/List.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { FaEdit, FaTrash } from "react-icons/fa";
+
+const List = () => {
+	return <div>Hello List</div>;
+};
+
+export default List;
