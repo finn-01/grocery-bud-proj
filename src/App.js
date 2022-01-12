acd52d896a41c8ccf585a66f5316d41421988af7
finn-01 <nguyensam576@gmail.com>
2022-01-12 16:51:54 +0700

    add

--- src/App.js
+++ src/App.js
@@ -12,7 +12,22 @@ const App = () => {
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		console.log("Hello World");
+		//console.log("Hello World");
+
+		if (!name) {
+			//display alert
+			//setAlert(true, "danger", "please enter value");
+			console.log("1");
+		} else if (name && isEditing) {
+			//deal with edit
+			console.log("2");
+		} else {
+			//add item
+			console.log("hi");
+			const newItem = { id: new Date().getTime().toString(), title: name };
+			setList([...list, newItem]);
+			setName("");
+		}
 	};
 
 	return (
@@ -33,10 +48,13 @@ const App = () => {
 					</button>
 				</div>
 			</form>
-			<div className="grocery-container">
-				<List />
-				<button className="clear-btn">clear items</button>
-			</div>
+
+			{list.length > 0 && (
+				<div className="grocery-container">
+					<List items={list} />
+					<button className="clear-btn">clear items</button>
+				</div>
+			)}
 		</section>
 	);
 };
